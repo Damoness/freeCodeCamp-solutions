b39e1f23dd650630da46d88a8c1de3ba4752bb7b
Damoness-Mini <qjwdcool@qq.com>
2019-10-30 22:01:45 +0800

    1.创建目录结构

--- createFile.js
+++ createFile.js
@@ -1,5 +1,7 @@
 var fs = require("fs");
 
+fs.constants.COPYFILE_FICLONE_FORCE
+
 let filePath =  process.argv[2];
 
 let dirPath =  filePath.replace('notes.md','');
@@ -11,9 +13,20 @@ const array = data.toString().split('\n');
 
 //console.log(array);
 
+//fs.writeFileSync(filePath,)
+
+let rewriteData = ''
+
 array.forEach((str,index)=>{
 
     let fileName = `${dirPath}${index+1}.${str}.html`;
+
+    //fs.writeFileSync(filePath,)
+
     fs.writeFileSync(fileName,'');
 
+    rewriteData += `## ${index+1}.${str}\n`
+
 })
+
+fs.writeFileSync(filePath,rewriteData);
